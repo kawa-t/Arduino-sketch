e268f26d7a9c39e703dfc7519d678064b6dd41f4
河村 泰佑 <kata.kawa813@gmail.com>
2022-11-27 00:34:57 +0900

    学校最強大会無限ループ追加

--- pokemon_sv/pokemon_sv_school_roop.ino
+++ pokemon_sv/pokemon_sv_school_roop.ino
@@ -3,11 +3,11 @@
 
 void setup() {
   // Switchがマイコンを認識するまでは信号を受け付けないため、適当な処理をさせておく
-  pushHat(Hat::DOWN, 2000, 5);
-  pushButton(Button::R, 2000);
+  pushHat(Button::A, 2000);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  pushButton(Button::A, 500);
+  pushButton(Button::A, 500, 4);
+  pushButton(Button::B, 500, 3);
 }
